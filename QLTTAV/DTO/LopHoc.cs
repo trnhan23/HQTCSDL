c6b9f10867d55097239fbcc04e62b34a567f2a63
Minh Tai <cavohin477@gronasu.com>
2023-10-23 20:28:22 +0700

    cai moi them 3 tang

---
+++ QLTTAV/DTO/LopHoc.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class LopHoc
+    {
+        public string MaLH {  get; set; }
+        public string TenLH { get; set; }
+        public string TenPhonghoc { get; set; }
+        public float HocPhi { get; set; }
+        public int SoBuoiHoc { get; set; }
+        public int SoLuongHV {  get; set; }
+        public string TrangThai {  get; set; }
+    }
+}
---
+++ QLTTAV/GUI/ChiNhanh.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace GUI
+{
+    partial class ChiNhanh
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnXoa1 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.txtTenCN = new System.Windows.Forms.TextBox();
+            this.txtMaCN = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lvChiNhanh = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnXoa1
+            // 
+            this.btnXoa1.Location = new System.Drawing.Point(720, 412);
+            this.btnXoa1.Name = "btnXoa1";
+            this.btnXoa1.Size = new System.Drawing.Size(156, 42);
+            this.btnXoa1.TabIndex = 11;
+            this.btnXoa1.Text = "Xoá";
+            this.btnXoa1.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(720, 338);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(156, 42);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Sửa";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // btnThem
+            // 
+            this.btnThem.Location = new System.Drawing.Point(720, 265);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(156, 42);
+            this.btnThem.TabIndex = 13;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = true;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.Location = new System.Drawing.Point(745, 169);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(208, 26);
+            this.txtDiaChi.TabIndex = 8;
+            // 
+            // txtTenCN
+            // 
+            this.txtTenCN.Location = new System.Drawing.Point(745, 99);
+            this.txtTenCN.Name = "txtTenCN";
+            this.txtTenCN.Size = new System.Drawing.Size(208, 26);
+            this.txtTenCN.TabIndex = 9;
+            // 
+            // txtMaCN
+            // 
+            this.txtMaCN.Location = new System.Drawing.Point(745, 29);
+            this.txtMaCN.Name = "txtMaCN";
+            this.txtMaCN.Size = new System.Drawing.Size(208, 26);
+            this.txtMaCN.TabIndex = 10;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(576, 169);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(57, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Địa chỉ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(576, 94);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Tên chi nhánh";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(576, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(104, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Mã chi nhánh";
+            // 
+            // lvChiNhanh
+            // 
+            this.lvChiNhanh.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.lvChiNhanh.GridLines = true;
+            this.lvChiNhanh.HideSelection = false;
+            this.lvChiNhanh.Location = new System.Drawing.Point(17, 18);
+            this.lvChiNhanh.Name = "lvChiNhanh";
+            this.lvChiNhanh.Size = new System.Drawing.Size(517, 594);
+            this.lvChiNhanh.TabIndex = 4;
+            this.lvChiNhanh.UseCompatibleStateImageBehavior = false;
+            this.lvChiNhanh.View = System.Windows.Forms.View.Details;
+            this.lvChiNhanh.SelectedIndexChanged += new System.EventHandler(this.lvChiNhanh_SelectedIndexChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Mã chi nhánh";
+            this.columnHeader1.Width = 100;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Tên chi nhánh";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Địa chỉ";
+            this.columnHeader3.Width = 150;
+            // 
+            // ChiNhanh
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1076, 524);
+            this.Controls.Add(this.btnXoa1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btnThem);
+            this.Controls.Add(this.txtDiaChi);
+            this.Controls.Add(this.txtTenCN);
+            this.Controls.Add(this.txtMaCN);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lvChiNhanh);
+            this.Name = "ChiNhanh";
+            this.Text = "ChiNhanh";
+            this.Load += new System.EventHandler(this.ChiNhanh_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnXoa1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnThem;
+        private System.Windows.Forms.TextBox txtDiaChi;
+        private System.Windows.Forms.TextBox txtTenCN;
+        private System.Windows.Forms.TextBox txtMaCN;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListView lvChiNhanh;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
